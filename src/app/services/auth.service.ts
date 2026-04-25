a6eee0a7b6e147bbfb8adf2e18b440c47ae7d925
muhamadumair <muhamadumair01@gmail.com>
2026-04-25 11:05:50 +0800

    Build the Authentication service to store the bearer token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private loginUrl = 'http://test-demo.aemenersol.com/api/account/login';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(this.loginUrl, credentials);
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('bearer_token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('bearer_token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  logout(): void {
+    localStorage.removeItem('bearer_token');
+  }
 }
