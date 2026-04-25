19c6b36d1f48c1f766338a42bf2ff3e3b8a73af5
muhamadumair <muhamadumair01@gmail.com>
2026-04-25 09:08:21 +0800

    Design the basic Login page UI together with defined typescript

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  isSubmitted = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  get f() { return this.loginForm.controls; }
+
+  onSubmit(): void {
+    this.isSubmitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    console.log('Form is valid! Data:', this.loginForm.value);
+  }
 }
